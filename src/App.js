45b1cd96ba0a3e1eebc03b0408f61496c16f6a70
phamhoangviet261 <hoangviet03082000@gmail.com>
2021-10-20 21:32:06 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+   BrowserRouter as Router,
+   Route,
+   Link,
+   Switch,
+   Redirect,
+   useLocation
+ } from "react-router-dom";
+import Cart from './components/Cart';
+import Header from './components/Header';
+import Product from './components/Product';
+import Homepage from './pages/Homepage'
+import Footer from './components/Footter';
+import ItemProduct from './components/ItemProduct';
+import './App.css'
+function App() {
+  return (
+     <Router>
+        <div className="containers">
+            <Header />
+            <Switch>
+               <Route path="/" exact component={Homepage} />
+               <Route path="/collections" exact component={Product} />
+               <Route path="/carts" exact component={Cart} />
+               <Route path="/product/:id" exact component={ItemProduct} />
+               <Route path="*">
+            <NoMatch />
+          </Route>
+            </Switch>
+            <Footer></Footer>
+        </div>
+     </Router>
+  );
+}
+
+function NoMatch() {
+   let location = useLocation();
+ 
+   return (
+     <div>
+       <h3>
+         No match for <code>{location.pathname}</code>
+       </h3>
+     </div>
+   );
+ }
+ 
+export default App;
